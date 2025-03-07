baa501b5c7dc4cff12b3c2560b9b01a259f6387d
Petrakowww <Petrakov.Egor.G@yandex.ru>
2025-03-07 09:28:59 +0300

    interceptor work

--- frontend/src/app/(pages)/(board)/dashboard/page.tsx
+++ frontend/src/app/(pages)/(board)/dashboard/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { api } from '@/shared/api';
+
 export default function Page() {
+
 	return (
 		<>
+
 			<section>
-				<h2 className="py-8 text-3xl font-bold text-center">Dashboard</h2>
+				<h2 className="py-8 text-3xl font-bold text-center">
+					Dashboard
+				</h2>
 			</section>
 		</>
 	);
