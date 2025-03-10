81c4682aeadd705be2a0df2715b797d7ea7ee8be
Petrakowww <Petrakov.Egor.G@yandex.ru>
2025-03-10 09:02:45 +0300

    del interceptor

--- frontend/src/shared/lib/utils/axios-interceptor.ts
+++ frontend/src/shared/lib/utils/axios-interceptor.ts
@@ -1,53 +1,55 @@
-import {
-	AxiosError,
-	AxiosInstance,
-	AxiosRequestConfig,
-	HttpStatusCode,
-} from 'axios';
-import { api } from '@/shared/api';
-import { authService } from '@/features/auth/services/auth.service';
-import { cookieClient } from './cookie.client';
+// TODO: не нужен, поскольку в Auth Guard на сервере уже есть проверка
 
-interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
-	_isRetry?: boolean;
-}
+// import {
+// 	AxiosError,
+// 	AxiosInstance,
+// 	AxiosRequestConfig,
+// 	HttpStatusCode,
+// } from 'axios';
+// import { api } from '@/shared/api';
+// import { authService } from '@/features/auth/services/auth.service';
+// import { cookieClient } from './cookie.client';
 
-class AuthInterceptor {
-	private axiosInstance: AxiosInstance;
+// interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
+// 	_isRetry?: boolean;
+// }
 
-	constructor(axiosInstance: AxiosInstance) {
-		this.axiosInstance = axiosInstance;
-		this.setupResponseInterceptor();
-	}
+// class AuthInterceptor {
+// 	private axiosInstance: AxiosInstance;
 
-	private setupResponseInterceptor() {
-		this.axiosInstance.interceptors.response.use(
-			(config) => config,
-			async (error: AxiosError) => {
-				const originalRequest =
-					error.config as AxiosRequestConfigWithRetry;
-				if (
-					error?.response?.status === HttpStatusCode.Unauthorized &&
-					originalRequest &&
-					!originalRequest._isRetry
-				) {
-					originalRequest._isRetry = true;
-					try {
-						await authService.refreshToken();
-					} catch (err) {
-						if (
-							(err as AxiosError).status ===
-							HttpStatusCode.Unauthorized
-						) {
-							cookieClient.logout();
-						}
-					}
-				}
+// 	constructor(axiosInstance: AxiosInstance) {
+// 		this.axiosInstance = axiosInstance;
+// 		this.setupResponseInterceptor();
+// 	}
 
-				throw error;
-			}
-		);
-	}
-}
+// 	private setupResponseInterceptor() {
+// 		this.axiosInstance.interceptors.response.use(
+// 			(config) => config,
+// 			async (error: AxiosError) => {
+// 				const originalRequest =
+// 					error.config as AxiosRequestConfigWithRetry;
+// 				if (
+// 					error?.response?.status === HttpStatusCode.Unauthorized &&
+// 					originalRequest &&
+// 					!originalRequest._isRetry
+// 				) {
+// 					originalRequest._isRetry = true;
+// 					try {
+// 						await authService.refreshToken();
+// 					} catch (err) {
+// 						if (
+// 							(err as AxiosError).status ===
+// 							HttpStatusCode.Unauthorized
+// 						) {
+// 							cookieClient.logout();
+// 						}
+// 					}
+// 				}
 
-export const authInterceptor = new AuthInterceptor(api.getInstance());
+// 				throw error;
+// 			}
+// 		);
+// 	}
+// }
+
+// export const authInterceptor = new AuthInterceptor(api.getInstance());
